216d27d874b762d373a55a47b127d6bb31d3c21a
jgsneves <jgsn.adv@gmail.com>
2021-07-21 14:17:49 -0300

    integration between front and back

--- packages/server/src/main.js
+++ packages/server/src/main.js
@@ -1,37 +1,25 @@
-import {createServer} from 'http';
-import {parse} from 'querystring';
+import express from 'express';
+import cors from 'cors';
 
-const server = createServer((request, response) => {
-    switch (request.url) {
-        case '/status': {
-            response.writeHead(200, {
-                'Content-Type': 'application/json',
-            });
-            response.write(JSON.stringify({
-                status: 'OKAY'
-            }));
-            response.end();
-            break;
-        }
-        case '/autenticate': {
-            let data = '';
-            request.on('data', (chunk) => {
-                data += chunk;
-            })
-            request.on('end', () => {
-                console.log(parse(data))
-                
-                response.end();
-            })
-            break;
-        }
-        default: {
-            response.writeHead(404, 'Service not found');
-            response.end();
-        }
-    }
+const server = express();
+
+server.use(cors());
+
+server.get('/status', (_, response) => {
+    response.send({
+        status: 'OK'
+    });
 });
 
+server.post('/authenticate', express.json(), (request, response) => {
+    console.log({
+        'email': request.body.email,
+        'senha': request.body.password
+    });
+    response.send({
+        status: 'ok'
+    });
+});
 
 server.listen(8000, '127.0.0.1', () => {
     console.log(`Server is running at http://127.0.0.1:8000`);
--- packages/web/src/pages/SignIn.jsx
+++ packages/web/src/pages/SignIn.jsx
@@ -1,10 +1,41 @@
 import React from 'react';
 
 const SignIn = () => {
+    const [email, setEmail] = React.useState('');
+    const [password, setPassword] = React.useState('');
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        fetch('http://127.0.0.1:8000/authenticate', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                email,
+                password,
+            })
+        }).then(response => response.json());
+    }
     return (
-        <form>
-            <input type="email" id="email" name="email" inputmode="email" autocomplete="username" />
-            <input type="password" id="password" name="password" inputmode="password" autocomplete="current-password" />
+        <form onSubmit={handleSubmit}>
+            <input 
+                type="email" 
+                id="email" 
+                name="email" 
+                inputMode="email" 
+                autoComplete="username" 
+                onChange={(event) => setEmail(event.target.value)} 
+                value={email}/>
+            <input 
+                type="password" 
+                id="password" 
+                name="password" 
+                inputMode="password" 
+                autoComplete="current-password" 
+                onChange={(event) => setPassword(event.target.value)}
+                value={password}
+                />
             <button type="submit">Entrar</button>
         </form>
     );
